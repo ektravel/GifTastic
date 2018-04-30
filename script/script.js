2cd583d849459219d265f1639eb7c650962d07e1
ektravel <38017980+ektravel@users.noreply.github.com>
2018-04-30 16:08:44 -0500

    updated img attribute value, added animate state

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 //Topics array to display when page loads
 var topics = ["broccoli", "cake", "muffin", "nachos", "tacos", "pizza", "noodles", "pasta", "potato", "apple", "pie"];
-//For loop to generate buttons for each array element
+
+//Loop to generate buttons for each array element
 for (i = 0; i < topics.length; i++) {
     var buttonHtml = "<button type='button' class='gifButton'>" + topics[i] + "</button>";
     $("#foodButtons").append(buttonHtml);
@@ -11,8 +12,9 @@ $(".gifButton").on("click", function() {
     var food = $(this).text();
     console.log(food);
     
-    //Construction a queryURL using the food name
+    //Construct a queryURL using the food name
     var queryURL = "https://api.giphy.com//v1/gifs/search?q=" + food + "&api_key=dc6zaTOxFJmzC&rating&limit=10";
+    
     //Performing an AJAX request with the queryURL
     $.ajax({
         url: queryURL,
@@ -21,25 +23,48 @@ $(".gifButton").on("click", function() {
         console.log(queryURL)
         console.log(response);
 
-        //Storing dta from the AJAX request in the results variable
+        //Storing data from the AJAX request in the results variable
         var results = response.data;
 
         //Looping through each result item
         for (i = 0; i < results.length; i++) {
-            //Creating div to hold individual gifs
             var foodDiv = $("<div>");
             //Creating paragraph tag to display gif rating
             var p = $("<p>").text("Rating: " + results[i].rating);
             //Creating image tag to display gifs
             var foodImage = $("<img>");
-            foodImage.attr("src", results[i].images.fixed_height.url);
+    
+            //Adding image src and data-still attribute to stop animation
+            foodImage.attr("src", results[i].images.fixed_height_still.url)
+            foodImage.attr("data-still", results[i].images.fixed_height_still.url)
+            foodImage.attr("data-animate", results[i].images.fixed_height.url)
+            foodImage.attr("data-state", "still")
+            foodImage.attr("class", "movGif")
+            
+            
+        
             //Adding paragraph and image tags to foodGifs div
             foodDiv.append(p);
             foodDiv.append(foodImage);
             //Prepanding the foodGifs to the HTML page in the foodGifs div
             $("#foodGifs").prepend(foodDiv);
-        }
-      });
-      
-  }
-);
+
+            //When gif img is clicked, the value of the attribute is updated
+            $(".movGif").on("click", function() {
+                
+                var state = $(this).attr("data-state");
+            //If the clicked image is state=still, change the attribute value to animate
+            if (state === "still") {
+                // $(this).attr("src", $(this).attr(results[i].images.fixed_height.url ));
+                $(this).attr("src", $(this).attr("data-animate") );
+                $(this).attr("data-state", "animate");
+            } 
+            else {
+                // $(this).attr("src", $(this).attr(results[i].images.fixed_height_still.url));
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+            }
+            });
+        };
+    });
+});
